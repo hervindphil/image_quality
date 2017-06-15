196e92e434901ff947e66f01ceb45947144d31f4
Hervind Philipe <hervind@olx.co.id>
2017-06-15 21:10:27 +0700

    Create feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,11 +2,17 @@ import numpy as np
 import cv2
 from scipy.signal import convolve2d
 
-height, width,_    = img.shape
-dist_pixel = len(np.unique([str(x) for x in img[:,:,:].reshape(-1, 3).tolist()]))
-dist_pixel_rate = dist_pixel / float(height * width)
+#image object is 'img'
 img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)   
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+#height and width
+height, width,_    = img.shape
+
+#distinct pixel rate
+dist_pixel = len(np.unique([str(x) for x in img[:,:,:].reshape(-1, 3).tolist()]))
+dist_pixel_rate = dist_pixel / float(height * width)
+
 
 #luminance
 luminance = img_gray.mean()
@@ -26,3 +32,8 @@ saturation = img_hsv[:,:,1].mean()
 red = img[:,:,2].mean()
 green = img[:,:,1].mean()
 blue = img[:,:,0].mean()
+
+red_var = img[:,:,2].var()
+green_var = img[:,:,1].var()
+blue_var = img[:,:,0].var()
+gray_var = img_gray.var()
